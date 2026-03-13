d260cbb6f55e134578892217617d958304cd4550
badige vamshi <badigevamshi@gmail.com>
2026-03-14 01:08:51 +0530

    Added add-to-cart animation and UI improvements

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 const hero = document.querySelector(".hero")
 const headset = document.querySelector(".headset-img")
-const spotlight = document.querySelector(".spotlight")
+const cartBtn = document.querySelector(".btn-cart")
+const cartIcon = document.querySelector(".cart")
+const cartCount = document.querySelector(".cart-count")
 
-hero.addEventListener("mousemove", (e) => {
+let count = 0
+
+/* 3D rotation */
+
+hero.addEventListener("mousemove",(e)=>{
 
 const rect = hero.getBoundingClientRect()
 
 const x = e.clientX - rect.left
 const y = e.clientY - rect.top
-
-/* 3D rotation */
 
 const rotateX = ((y / rect.height) - 0.5) * 18
 const rotateY = ((x / rect.width) - 0.5) * -18
@@ -17,15 +21,44 @@ const rotateY = ((x / rect.width) - 0.5) * -18
 headset.style.transform =
 `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
 
-/* spotlight follow */
-
-spotlight.style.left = x + "px"
-spotlight.style.top = y + "px"
-
 })
 
-hero.addEventListener("mouseleave", () => {
+hero.addEventListener("mouseleave",()=>{
+headset.style.transform="rotateX(0deg) rotateY(0deg)"
+})
 
-headset.style.transform = "rotateX(0deg) rotateY(0deg)"
+/* ADD TO CART ANIMATION */
+
+cartBtn.addEventListener("click",()=>{
+
+const img = headset.cloneNode(true)
+const rect = headset.getBoundingClientRect()
+
+img.classList.add("fly-img")
+
+img.style.left = rect.left + "px"
+img.style.top = rect.top + "px"
+
+document.body.appendChild(img)
+
+const cartRect = cartIcon.getBoundingClientRect()
+
+setTimeout(()=>{
+
+img.style.left = cartRect.left + "px"
+img.style.top = cartRect.top + "px"
+img.style.width = "30px"
+img.style.opacity = "0.5"
+
+},10)
+
+setTimeout(()=>{
+
+img.remove()
+
+count++
+cartCount.textContent = count
+
+},800)
 
 })
