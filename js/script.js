8c67717da1a9f3b02c64da0d3181faec09be0ca8
badige vamshi <badigevamshi@gmail.com>
2026-03-14 01:26:46 +0530

    Final working version

--- js/script.js
+++ js/script.js
@@ -1,12 +1,6 @@
 const hero = document.querySelector(".hero")
 const headset = document.querySelector(".headset-img")
-const cartBtn = document.querySelector(".btn-cart")
-const cartIcon = document.querySelector(".cart")
-const cartCount = document.querySelector(".cart-count")
-
-let count = 0
-
-/* 3D rotation */
+const spotlight = document.querySelector(".spotlight")
 
 hero.addEventListener("mousemove",(e)=>{
 
@@ -15,50 +9,19 @@ const rect = hero.getBoundingClientRect()
 const x = e.clientX - rect.left
 const y = e.clientY - rect.top
 
-const rotateX = ((y / rect.height) - 0.5) * 18
-const rotateY = ((x / rect.width) - 0.5) * -18
+const rotateX = ((y/rect.height)-0.5)*20
+const rotateY = ((x/rect.width)-0.5)*-20
 
 headset.style.transform =
 `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
 
+spotlight.style.left = x+"px"
+spotlight.style.top = y+"px"
+
 })
 
 hero.addEventListener("mouseleave",()=>{
-headset.style.transform="rotateX(0deg) rotateY(0deg)"
-})
 
-/* ADD TO CART ANIMATION */
-
-cartBtn.addEventListener("click",()=>{
-
-const img = headset.cloneNode(true)
-const rect = headset.getBoundingClientRect()
-
-img.classList.add("fly-img")
-
-img.style.left = rect.left + "px"
-img.style.top = rect.top + "px"
-
-document.body.appendChild(img)
-
-const cartRect = cartIcon.getBoundingClientRect()
-
-setTimeout(()=>{
-
-img.style.left = cartRect.left + "px"
-img.style.top = cartRect.top + "px"
-img.style.width = "30px"
-img.style.opacity = "0.5"
-
-},10)
-
-setTimeout(()=>{
-
-img.remove()
-
-count++
-cartCount.textContent = count
-
-},800)
+headset.style.transform = "rotateX(0deg) rotateY(0deg)"
 
 })
